409274812dadb0a90ee8f47e234e28ebe9f57bbd
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-08-23 10:26:28 -0700

    Initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Search from '../Components/Search'
+import IntroPost from '../Components/IntroPost'
+import Blogs from '../Components/Blogs'
+import Footer from '../Components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Search/>
+        <IntroPost/>
+        <Blogs/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
